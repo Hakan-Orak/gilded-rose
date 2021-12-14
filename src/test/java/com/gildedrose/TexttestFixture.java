d46a7e90e5694c6fe9fa5564aa7132415cd70111
faycal-rakza <faycal.rakza@epsi.fr>
2021-12-14 12:20:22 +0100

    Changes by hakan

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -4,6 +4,8 @@ import entities.Item;
 
 public class TexttestFixture {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Item itemOne = new Item("Anneau de Doran", 15, 10, "commun");
+        System.out.println(itemOne.toString());
+
     }
 }
